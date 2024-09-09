cdf236a4c60f87293011e80e0cd29e873e83393b
locDEV169 <loc16910@gmail.com>
2024-09-09 23:15:28 +0700

    config theme and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,63 @@
-import { Button, Layout } from "antd";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Button, ConfigProvider, Layout } from "antd";
 import { useState } from "react";
-import SiderBar from "./components/siderBar/sidebar";
 import "./App.css";
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
 import CustomHeader from "./components/header/sidebar";
+import SiderBar from "./components/siderBar/sidebar";
 
 const { Sider, Header, Content } = Layout;
+const themeConfig = {
+  token: {
+    // Seed Token
+    colorPrimary: "#4f6f52",
+    borderRadius: 2,
+
+    // Alias Token
+    colorBgContainer: "#f6ffed",
+  },
+  components: {
+    Input: {
+      borderRadius: 10,
+      colorInfoBorderHover: "#4f6f52",
+    },
+    Button: {
+      colorPrimary: "#4f6f52",
+      algorithm: true, // Enable algorithm
+    },
+  },
+};
+
 const App = () => {
   const [collapsed, setCollapsed] = useState<boolean>(false);
-
   return (
-    <Layout>
-      <Sider
-        theme="light"
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        className="sider"
-      >
-        <SiderBar />
-        <Button
-          type="text"
-          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          className="trigger-btn"
-          onClick={() => setCollapsed(!collapsed)}
-        ></Button>
-      </Sider>
+    <ConfigProvider
+      componentSize={"middle"}
+      theme={themeConfig}
+    >
       <Layout>
-        <Header className="header">
-          <CustomHeader />
-        </Header>
-        <Content className="content"></Content>
+        <Sider
+          theme="light"
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
+          className="sider"
+        >
+          <SiderBar />
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            className="trigger-btn"
+            onClick={() => setCollapsed(!collapsed)}
+          ></Button>
+        </Sider>
+        <Layout>
+          <Header className="header">
+            <CustomHeader />
+          </Header>
+          <Content className="content"></Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 };
 
